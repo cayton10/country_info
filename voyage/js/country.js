b827908f3b4c8428e2da296fbc9e7e00ddbe540c
Obi-Wan <cayton10@live.marshall.edu>
2020-03-08 15:14:34 -0400

    just needs typeahead now

--- voyage/js/country.js
+++ voyage/js/country.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
     dropdown.append('<option selected="true" disabled>Choose Capital City</option>');
 
     /* ------------------- restCountries API for ALL countries ------------------ */
-
-    const allCity = 'https://restcountries.eu/rest/v2/all'
-
+              //Filter results for only what is needed for this application//
+    const allCity = 'https://restcountries.eu/rest/v2/all?fields=name;capital;currencies;flag;population'
+    const allName = 'https://restcountries.eu/rest/v2/all?fields=name' 
     /* --------------------- populate dropdown w/ json data --------------------- */
     /* ------------------- $.getJSON() shorthand for $.ajax() ------------------- */
 
@@ -36,7 +36,7 @@ $(document).ready(function(){
             if (entry.capital !== ''){
                 dropdown.append($('<option></option>').text(entry.capital));//Append to select <option>s
             }
-            //Function to populate country name text input field
+            //Function to populate #country-name text input field and other info
             $('#capital-dropdown').change(function (){
                 var city = $(this).val(); //Store selected capital in variable 'city'
                 $.ajax(
@@ -52,9 +52,11 @@ $(document).ready(function(){
                                 if (city == entry.capital) //If selected capital city == capital from API...
                                 {   //Spit out all of this info on identified tabs
                                     $('#country-text').val(entry.name);
-                                    $('#currency').html(entry.currencies[0].code + '<br>' + entry.currencies[0].name + '<br>' + entry.currencies[0].symbol) ;
+                                    $('#currency-code').val('Code: ' + entry.currencies[0].code);
+                                    $('#currency-name').val('Name: ' + entry.currencies[0].name);
+                                    $('#currency-sym').val('Symbol: ' + entry.currencies[0].symbol);
                                     $('#flag').html("<img class='added-img' src='" + entry.flag + "' />");
-                                    $('#pop').html(entry.name + ' population: ' + entry.population.toLocaleString("en-US"));
+                                    $('#pop').val(entry.name + ' population: ' + entry.population.toLocaleString("en-US"));
                                                                                 //Found '.toLocaleString' on Stack Overflow forum
                                                                                 //https://stackoverflow.com/questions/52795097/json-numbers-formatted-with-commas
                                 }
@@ -65,12 +67,83 @@ $(document).ready(function(){
         });
     });
 
-/* --------------------- country-dropdown event listener -------------------- */
-    $('#country-dropdown').change(function (){
-        //Capture dropdown value into variable
-        var countryName = $(this).val();
-        $.ajax()
-    })
+    /* -------------------------------------------------------------------------- */
+    /*                             TEXT INPUT FUNCTION                            */
+    /* -------------------------------------------------------------------------- */
+    
+    /*var countries = new Bloodhound({
+        datumTokenizer: function(countries) {
+            return Bloodhound.tokenizers.whitespace(countries.value);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        limit: 3,
+        prefetch: allName,
+        remote: {
+          url: allName,
+          wildcard: '%QUERY',
+          filter: function(response){
+              return response.countries;
+          }
+        }
+      });
+
+      // init suggestion engine
+      countries.initialize();
+      
+      $('#country-text').typeahead(
+        {   hint: true, 
+            highlight: true,
+            minLength: 1
+        },
+        {
+          name: 'countries',
+          displayKey: function(countries) {
+              return countries.name;
+          },
+        });
+        source: countries.ttAdapter()*/
 
 
+        //Prevent default 'enter' keystroke event//
+        $('#country-text').keypress(function (event){
+            //Keycode 13 == 'return or enter'
+            //Must preventDefault to keep from reloading page
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                var country = $('#country-text').val();
+                console.log(country);
+            }
+            //If user presses enter key...
+            if (event.keyCode === 13) {
+                $.ajax(//Ajax call to restcountries
+                    {
+                        //Partial name version does not work since results are specific
+                        url: 'https://restcountries.eu/rest/v2/name/' + country + '?fullText=true',
+                        dataType: 'json',
+                        method: 'get',
+                        data: 'none',
+                        success: function(data)
+                        {
+                            $.each(data, function(key, entry)//iterate through countries API. 
+                            {       //Ignore case
+                                if (country.toLowerCase() == entry.name.toLowerCase()) //If selected capital city == capital from API...
+                                {   //Spit out all of this info on identified tabs
+                                    $('#capital-dropdown').val(entry.capital);
+                                    $('#currency-code').val('Code: ' + entry.currencies[0].code);
+                                    $('#currency-name').val('Name: ' + entry.currencies[0].name);
+                                    $('#currency-sym').val('Symbol: ' + entry.currencies[0].symbol);
+                                    $('#flag').html("<img class='added-img' src='" + entry.flag + "' />");
+                                    $('#pop').val(entry.name + ' population: ' + entry.population.toLocaleString("en-US"));
+                                                                               
+                                }
+                            });
+                        },
+                        //error handling for misspelled or other
+                        error: function(xhr){
+                            alert(xhr.status + ": No country by that name. You can check your spelling here: " + allName);
+                        }
+                         
+                    });
+            }
+        });
 });
